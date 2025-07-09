7060715c2f8eb6b5489102fde4e8cf884a098560
vikanemchynovich <viktoria.nemchynovich@gmail.com>
2025-07-09 13:09:45 +0300

    Add files via upload

---
+++ BinarySearch_5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+
+    int N, step;
+    cout << "Введите длину массива: ";
+    cin >> N;
+    cout << "Введите шаг: ";
+    cin >> step;
+
+    vector<int> arr;
+    int jump_pos = rand() % (N - 2) + 1;
+
+    for (int i = 0, val = 0; i < N; i++) {
+        arr.push_back(val);
+        val += (i == jump_pos) ? 2 * step : step;
+    }
+
+    cout << "Массив: ";
+    for (int num : arr) cout << num << " ";
+    cout << endl;
+
+    int left = 0, right = N - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == arr[0] + mid * step) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    int missing = arr[0] + left * step;
+
+    cout << "Недостающий элемент: " << missing << endl;
+    cout << "Позиция прыжка: " << left << endl;
+
+    return 0;
+}
---
+++ FileBMP.cpp
@@ -0,0 +1,67 @@
+﻿#include<iostream>
+#include<fstream>
+using namespace std;
+
+struct color { unsigned char r; unsigned char g; unsigned char b; };
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    ifstream fff("1.bmp", ios::binary);
+    ofstream ggg("rez.bmp", ios::binary);
+    if (!fff) { cout << "No file 1.bmp. Can't open\n"; exit(1); }
+    if (!ggg) { cout << "   file rez.bmp. Can't create\n"; exit(1); }
+
+    char buf[100];
+    color bg, rect;
+    int w, h;
+
+    fff.read((char*)&buf, 18);
+    ggg.write((char*)&buf, 18);
+    fff.read((char*)&w, 4); cout << "w=" << w;
+    fff.read((char*)&w, 4); cout << ", h=" << w;
+    w = 256; ggg.write((char*)&w, 4);
+    h = 256; ggg.write((char*)&h, 4);
+    fff.read((char*)&buf, 28);
+    ggg.write((char*)&buf, 28);
+
+    cout << "\nbackground color\n";
+    int cr, cg, cb;
+    cin >> cb;
+    cin >> cg;
+    cin >> cr;
+
+    bg.r = cr; bg.g = cg; bg.b = cb;
+    cout << "\nfigure color\n";
+
+    cin >> cb;
+    cin >> cg;
+    cin >> cr;
+
+    rect.r = cr; rect.g = cg; rect.b = cb;
+    int point_x, point_y, size_width, size_height;
+    cout << "\npoint coordinate(x, y)\n";
+    cin >> point_x;
+    cin >> point_y;
+    cout << "\nsize of figure(width, height)\n";
+    cin >> size_width;
+    cin >> size_height;
+
+    for (int cartesian_y = 0; cartesian_y < h; ++cartesian_y) {
+        for (int cartesian_x = 0; cartesian_x < w; ++cartesian_x) {
+            if (cartesian_x >= point_x &&
+                cartesian_x < point_x + size_width &&
+                cartesian_y >= point_y &&
+                cartesian_y < point_y + size_height) {
+                ggg.write((char*)&rect, sizeof(rect));
+            }
+            else {
+                ggg.write((char*)&bg, sizeof(bg));
+            }
+        }
+    }
+
+    fff.close();
+    ggg.close();
+    cout << "Image created successfully!\n";
+    return 0;
+}
